33bc65def52c453c75c2b9283ecc348dc8cf9aae
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-03-22 17:33:39 +0530

    .

--- src/main/java/com/learn/entities/User.java
+++ src/main/java/com/learn/entities/User.java
@@ -1,5 +1,32 @@
 package com.learn.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer userId;
+	
+	@Column(name="User_name" , nullable = false ,length= 100)
+	private String email;
+	private String password;
+	private String about;
+	
+	
 }
