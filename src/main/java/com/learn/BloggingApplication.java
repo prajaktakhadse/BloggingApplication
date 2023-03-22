a4dd6933eb300fb6923d77f1f0c086842e1d30e4
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-03-22 10:32:08 +0530

    upddated

--- src/main/java/com/learn/BloggingApplication.java
+++ src/main/java/com/learn/BloggingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BloggingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
+		SpringApplication.run(BloggingApplication.class, args);
 	}
 
 }
