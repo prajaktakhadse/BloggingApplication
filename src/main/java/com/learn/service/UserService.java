3f99cf361ea5b18852caca4680f74436b3f06a6e
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-03-23 22:00:22 +0530

    .

--- src/main/java/com/learn/service/UserService.java
+++ src/main/java/com/learn/service/UserService.java
@@ -6,7 +6,7 @@ import com.learn.payload.UserDtos;
 public interface UserService {
 
 	UserDtos createUser(UserDtos user);
-	UserDtos updateUser(UserDtos user ,Integer userId);
+	UserDtos updateUser(UserDtos userDto ,Integer userId);
 	UserDtos getUser(Integer userId);
 	List<UserDtos> getAllUsers();
 	void deleteUser(Integer UserId);
