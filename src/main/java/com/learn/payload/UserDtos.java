66e06d7d574135e98a44c1b6c66e8b4f8ce9ea70
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-03-24 15:27:44 +0530

    Update UserDtos.java

--- src/main/java/com/learn/payload/UserDtos.java
+++ src/main/java/com/learn/payload/UserDtos.java
@@ -3,6 +3,7 @@ package com.learn.payload;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,19 +15,20 @@ import lombok.Setter;
 public class UserDtos {
 	
 	private Integer userId;
-	@NotEmpty
+	@NotEmpty @NotNull
 	@Size(min = 4, message = "Username must be min of 4 characters !!")
 	private String name;
 	
 	@Email(message = "Email address is not valid !!")
 	@NotEmpty(message = "Email is required !!")
+	@NotNull
 	private String email;
 	
 
-	@NotEmpty
+	@NotEmpty @NotNull
 	@Size(min = 3, max = 10, message = "Password must be min of 3 chars and max of 10 chars !!")
 	private String password;
 	
-	@NotEmpty
+	@NotEmpty @NotNull
 	private String about;
 }
