5b16cdbac988a38ca98b50f8dd6556f7cb50b0bd
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-03-24 14:39:54 +0530

    Update UserController.java

--- src/main/java/com/learn/controller/UserController.java
+++ src/main/java/com/learn/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,7 @@ public class UserController {
 		return new ResponseEntity<UserDtos>(createUserDtos, HttpStatus.CREATED);
 	}
 	
+	@PutMapping("/{userId}")
 	public ResponseEntity<UserDtos> updateUser(@Valid @RequestBody UserDtos userDtos,  @PathVariable("userId")Integer uid){
 		UserDtos updateUser = this.userService.updateUser(userDtos, uid);
 		return ResponseEntity.ok(updateUser);
