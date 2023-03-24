ddc8b7ac5e869d043443376f65bcfffa7ca6b0c2
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-03-24 20:06:48 +0530

    Update User.java

--- src/main/java/com/learn/entities/User.java
+++ src/main/java/com/learn/entities/User.java
@@ -1,10 +1,16 @@
 package com.learn.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
@@ -29,5 +35,8 @@ public class User {
 	private String password;
 	private String about;
 	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
+
 	
 }
