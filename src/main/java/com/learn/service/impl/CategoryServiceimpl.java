8c5fd54215df5d96055b3fda4dd46cef35d9ba6b
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-03-24 15:52:15 +0530

    Update CategoryServiceimpl.java

--- src/main/java/com/learn/service/impl/CategoryServiceimpl.java
+++ src/main/java/com/learn/service/impl/CategoryServiceimpl.java
@@ -1,5 +1,58 @@
 package com.learn.service.impl;
 
-public class CategoryServiceimpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.learn.entities.Category;
+import com.learn.exception.ResourceNotFoundException;
+import com.learn.payload.CategoryDto;
+import com.learn.repository.CategoryRepo;
+import com.learn.service.CategoryService;
+
+public class CategoryServiceimpl  implements CategoryService{
+
+	@Autowired
+	private CategoryRepo categoryRepo;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Override
+	public CategoryDto createCategory(CategoryDto categoryDto) {
+		Category cat = this.modelMapper.map(categoryDto, Category.class);
+		Category addedCat = this.categoryRepo.save(cat);
+		return this.modelMapper.map(addedCat, CategoryDto.class);
+	}
+
+	@Override
+	public CategoryDto updateCategory(CategoryDto categoryDto, Integer categoryId) {
+		Category cat = this.categoryRepo.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("Category","categoryId", categoryId));
+		cat.setCategoryTitle(categoryDto.getCategoryTitle());
+		cat.setCategoryDescription(categoryDto.getCategoryDescription());
+		Category updatedCate = this.categoryRepo.save(cat);
+		
+		return this.modelMapper.map(updatedCate,CategoryDto.class);
+	}
+
+	@Override
+	public void deleteCategory(Integer categoryId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public CategoryDto getCategory(Integer categoryId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CategoryDto> getCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
