3006640296225fdcde0ea6b32c20b03bda77bfc6
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-03-24 14:46:15 +0530

    Update BloggingApplicationTests.java

--- src/test/java/com/learn/BloggingApplicationTests.java
+++ src/test/java/com/learn/BloggingApplicationTests.java
@@ -3,6 +3,8 @@ package com.learn;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
 @SpringBootTest
 class BloggingApplicationTests {
 
@@ -10,4 +12,14 @@ class BloggingApplicationTests {
 	void contextLoads() {
 	}
 
+//	@Autowired
+//	private UserRepo userRepo;
+//	
+//	@Test
+//	public void repoTest() {
+//		String className = this.userRepo.getClass().getName();
+//		String packName = this.userRepo.getClass().getPackageName();
+//		System.out.println(className);
+//		System.out.println(packName);
+//	}
 }
