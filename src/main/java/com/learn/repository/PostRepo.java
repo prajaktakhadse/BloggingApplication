070e07fb812dc81e8b37dcf0877cb837fce9b9ba
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-03-25 13:49:52 +0530

    .

--- src/main/java/com/learn/repository/PostRepo.java
+++ src/main/java/com/learn/repository/PostRepo.java
@@ -3,6 +3,8 @@ package com.learn.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.learn.entities.Category;
 import com.learn.entities.Post;
@@ -12,4 +14,8 @@ public interface PostRepo extends JpaRepository<Post, Integer> {
 
 	List<Post> findByUser(User user);
 	List<Post> findByCategory(Category category);	
+	
+	@Query("select p from Post p where p.title like :key")
+	List<Post> searchByTitle(@Param("key") String title);
+	
 }
--- src/main/java/com/learn/service/impl/PostServiceImpl.java
+++ src/main/java/com/learn/service/impl/PostServiceImpl.java
@@ -109,8 +109,9 @@ public class PostServiceImpl implements PostService {
 
 	@Override
 	public List<PostDto> searchPosts(String keyword) {
-		// TODO Auto-generated method stub
-		return null;
+		    List<Post> posts = this.postRepo.searchByTitle("%" + keyword + "%");
+	        List<PostDto> postDtos = posts.stream().map((post) -> this.modelMapper.map(post, PostDto.class)).collect(Collectors.toList());
+	        return postDtos;
 	}
 
 }
