f1d5496f40181da3e9fcab7f00271e3d1c326a2b
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-04-07 00:53:26 +0530

    security updated basic authentication

---
+++ src/main/java/com/learn/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.learn.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.
+		csrf()
+		.disable()
+		.authorizeHttpRequests()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
+		
+	}
+
+}
