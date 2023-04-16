6c5257a77fae31e19c4f3918cb8e5c4137bffe89
Prajakta_Khadse <prajaktakhadse00@gmail.com>
2023-04-16 23:17:24 +0530

    swagger

--- src/main/java/com/learn/config/SecurityConfig.java
+++ src/main/java/com/learn/config/SecurityConfig.java
@@ -23,6 +23,17 @@ import com.learn.security.CustomUserDetailsService;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	public static final String[] PUBLIC_URLS = {
+			"/api/v1/auth/**",
+			"/v3/api-docs",
+			"/v2/api-docs",
+			"/swagger-resources/**",
+			"/swagger-ui/**",
+			"/webjars/**"
+			
+	};
+	
+	
 	@Autowired
 	private CustomUserDetailsService customUserDetailService;
 	
@@ -44,10 +55,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.cors()
 		.and()
 		.authorizeHttpRequests()
-		.antMatchers("/api/v1/auth/login")
+		.antMatchers("PUBLIC_URLS")
 		.permitAll()
-		.antMatchers("/v3/api-docs")
-		.permitAll()
+		
 		.anyRequest()
 		.authenticated()
 		.and()
